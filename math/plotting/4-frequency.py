627b67240dd7005fec2c1986a2f9cb16c491b21d
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 17:07:51 -0400

    Task 4: Simplified histogram with explicit bins variable

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,16 +10,18 @@ import matplotlib.pyplot as plt
 def frequency():
     """
     Plots a histogram of student grades for Project A.
-    
+
     Creates a histogram showing the distribution of 50 student grades
     with bins every 10 units from 0 to 100, outlined in black.
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
-    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.xlim(0, 100)
     plt.show()
