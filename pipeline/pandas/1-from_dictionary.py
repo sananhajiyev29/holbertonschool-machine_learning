64308eb09b11e574932cdd2fe98adc213271292d
sananhajiyev29 <12681holbertonstudents.com>
2026-01-10 14:23:54 +0000

    Task 1: create DataFrame from dictionary

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Creates a pandas DataFrame from a dictionary."""
+
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "First": [0.0, 0.5, 1.0, 1.5],
+        "Second": ["one", "two", "three", "four"],
+    },
+    index=["A", "B", "C", "D"],
+)
