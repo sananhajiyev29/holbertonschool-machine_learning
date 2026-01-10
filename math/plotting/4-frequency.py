c39c788289e9b5d0e988ec2c461ee0ec134e489a
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 15:03:50 -0400

    Task 4: Fix histogram using bins=10 to match reference

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -15,13 +15,14 @@ def frequency():
     - x-axis: Grades
     - y-axis: Number of Students
     - Title: Project A
-    - Bins every 10 units
+    - 10 bins
     - Bars outlined in black
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=10, edgecolor='black')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
--- math/plotting/4-main.py
+++ math/plotting/4-main.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""
-Main file to run Task 4: Frequency
-"""
 
 frequency = __import__('4-frequency').frequency
 
