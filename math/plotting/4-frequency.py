8ce04f555c9c7f0c516869dd49a60edfbc68553c
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 17:17:05 -0400

    Task 4: Using bins=10 with range parameter

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -18,10 +18,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xlim(0, 100)
     plt.show()
