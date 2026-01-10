a27b7349edaf9215c9955d01d2bb31c08d96634a
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 17:39:57 -0400

    Task 8: 3D PCA visualization of Iris dataset

---
+++ math/plotting/101-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+# No function to import - the script runs directly
+exec(open('101-pca.py').read())
---
+++ math/plotting/101-pca.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Task 8: PCA
+Visualizes the Iris dataset in 3D using Principal Component Analysis.
+"""
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+lib = np.load("pca.npz")
+data = lib["data"]
+labels = lib["labels"]
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+pca_data = np.matmul(norm_data, Vh[:3].T)
+
+fig = plt.figure(figsize=(6.4, 4.8))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2], c=labels, cmap='plasma')
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+ax.set_title('PCA of Iris Dataset')
+
+plt.show()
