a0620afe447676cbb400a3141f400db8ae33232b
sananhajiyev29 <12681holbertonstudents.com>
2026-01-10 14:59:38 +0000

    Task 11: concatenate df2 subset on top of df1 with keys

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Concatenate two DataFrames with Timestamp indexing and keys."""
+
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    Index both DataFrames on Timestamp, select subset of df2,
+    and concatenate on top of df1 with keys.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+    df2_subset = df2[df2.index <= 1417411920]
+    df_concat = pd.concat([df2_subset, df1], keys=["bitstamp", "coinbase"])
+    return df_concat
