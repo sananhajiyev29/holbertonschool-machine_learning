e251656deec9c3afb22787148133f4436e75a2af
sananhajiyev29 <12681holbertonstudents.com>
2026-01-10 15:05:45 +0000

    Task 12: hierarchy function with Timestamp as first MultiIndex level

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Concatenate DataFrames with Timestamp as first level in MultiIndex."""
+
+import pandas as pd
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """
+    Index both DataFrames on Timestamp, select range of timestamps,
+    concatenate with keys, and sort in chronological order.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df1_subset = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
+    df2_subset = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
+
+    df_concat = pd.concat([df2_subset, df1_subset], keys=["bitstamp", "coinbase"])
+    df_concat = df_concat.reorder_levels([1, 0]).sort_index()
+    return df_concat
