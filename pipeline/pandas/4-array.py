17cd9233d214665dedf1f273b1ffea56dd5cf27a
sananhajiyev29 <12681holbertonstudents.com>
2026-01-10 14:37:06 +0000

    Fix Task 4: remove forbidden import

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Converts selected DataFrame values to a NumPy array."""
 
-import pandas as pd
-
 
 def array(df):
     """Returns a NumPy array of the last 10 High and Close values."""
