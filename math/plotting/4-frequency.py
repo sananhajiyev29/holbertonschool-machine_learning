f5f7551a3ec7f0fd1f3726d7304580c5df29f37e
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 17:19:16 -0400

    Task 4: Minimal histogram implementation

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,13 +12,13 @@ def frequency():
     Plots a histogram of student grades for Project A.
 
     Creates a histogram showing the distribution of 50 student grades
-    with bins every 10 units from 0 to 100, outlined in black.
+    with bins every 10 units, outlined in black.
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
