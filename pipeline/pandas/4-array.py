aa100cc88aa6998dcad8c9ad1a6609f87286dead
sananhajiyev29 <12681holbertonstudents.com>
2026-01-10 14:35:17 +0000

    Task 4: convert DataFrame to NumPy array

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Converts selected DataFrame values to a NumPy array."""
+
+import pandas as pd
+
+
+def array(df):
+    """Returns a NumPy array of the last 10 High and Close values."""
+    return df[['High', 'Close']].tail(10).to_numpy()
