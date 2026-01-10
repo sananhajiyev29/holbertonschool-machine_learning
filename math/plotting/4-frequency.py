c8b0940e7859ee10b15f08a9d60a7e26ddcb991a
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 15:46:21 -0400

    Task 4 Fixed 2: Frequency histogram

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,11 +6,17 @@ def frequency():
     """Plot a histogram of student grades for Project A."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
 
-    bins = range(0, 101, 10)  # bins every 10 units
+    # bins every 10 units from 0 to 100
+    bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    
+    # histogram with black edges
     plt.hist(student_grades, bins=bins, edgecolor='black')
-
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
+    
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
     # Do not call plt.show() for autograder
+
+    plt.show()
