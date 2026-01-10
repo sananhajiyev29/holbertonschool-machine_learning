e387f7820efdcbbbe54332a245b2341da9ed4500
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 16:27:16 -0400

    Task 4 Fixed v2: Frequency histogram with proper axis limits

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -18,10 +18,10 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
     
-    plt.xlim(0, 100)
-    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+    plt.hist(student_grades, bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], edgecolor='black')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
+    plt.xlim(0, 100)
     
     plt.show()
