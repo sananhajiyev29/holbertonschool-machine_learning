debfa853f6008185dfc03ca5791e7117b03ea056
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 16:57:28 -0400

    Task 4: Added proper documentation to histogram

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
+"""
+Task 4: Frequency
+Plots a histogram of student grades for Project A.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
+    """
+    Plots a histogram of student grades for Project A.
+    
+    Creates a histogram showing the distribution of 50 student grades
+    with bins every 10 units from 0 to 100, outlined in black.
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
