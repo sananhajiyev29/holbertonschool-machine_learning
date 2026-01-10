67fb914e375614a0ef5c82ad1dbcf057d990d5b3
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-10 16:34:31 -0400

    Task 4 Fixed: Frequency histogram with proper axis limits of every bin

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
-"""
-Task 4: Frequency
-Plots a histogram of student grades for Project A.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """
-    Plots a histogram of student grades for Project A.
-    
-    Creates a histogram showing the distribution of 50 student grades
-    with bins every 10 units, outlined in black.
-    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
+
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
