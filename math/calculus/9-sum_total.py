ffda76f3bffa52b87e2d83e31b97bb1ee9fa63c4
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-12 18:32:53 +0000

    Task 9 Fixed: Sum of squares function without loops

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Calculate the sum of squares from 1 to n without loops."""
 
+
 def summation_i_squared(n):
     """Return the sum of squares from 1 to n or None if n is invalid."""
     if not isinstance(n, int) or n < 1:
