4854e5e38b3222b64d500473d7eb8b5e3fecd029
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-12 18:37:08 +0000

    Task 10 Fixed: Polynomial derivative function

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,9 +4,9 @@
 
 def poly_derivative(poly):
     """Return the derivative of a polynomial as a list of coefficients."""
-    if not isinstance(poly, list) or not all(isinstance(x, (int, float)) for x in poly):
+    if not isinstance(poly, list) or not poly or not all(isinstance(x, (int, float)) for x in poly):
         return None
-    if len(poly) <= 1:  # derivative of constant is 0
+    if len(poly) == 1:  # derivative of constant is 0
         return [0]
     # derivative: multiply each coefficient by its power (skip constant at index 0)
     derivative = [i * poly[i] for i in range(1, len(poly))]
