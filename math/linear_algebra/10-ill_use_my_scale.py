9ff2440e5ba93fc1d4ab2826f274b63a7cb3265c
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-19 19:29:24 +0000

    Add function to get numpy array shape

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to get the shape of a numpy ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray as a tuple of integers.
+    """
+    return matrix.shape
