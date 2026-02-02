df0c2e0ebec23f988d407dca69379c2de27ad2a9
sananhajiyev29 <12681@holbertonstudents.com>
2026-02-02 19:08:15 +0000

    1. Poisson PMF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,9 +1,32 @@
+#!/usr/bin/env python3
+"""
+Poisson distribution module
+"""
+
+
+class Poisson:
+    """
+    Represents a Poisson distribution
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes a Poisson distribution
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
     def pmf(self, k):
         """
         Calculates the value of the PMF for a given number of successes
-
-        k: number of successes
-        Returns: PMF value for k
         """
         if not isinstance(k, int):
             k = int(k)
