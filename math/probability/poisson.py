8b1bfc3f17ce92f1ddeedef532f09f69dcd22120
sananhajiyev29 <12681@holbertonstudents.com>
2026-02-02 19:12:33 +0000

    2. Poisson CDF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -9,7 +9,39 @@ class Poisson:
     Represents a Poisson distribution
     """
 
-        def cdf(self, k):
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes a Poisson distribution
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        return (e ** (-self.lambtha)) * (self.lambtha ** k) / factorial
+
+    def cdf(self, k):
         """
         Calculates the value of the CDF for a given number of successes
         """
