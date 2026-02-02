223cd9feb84a91db0ce5afb2c9b0ff054971c91b
sananhajiyev29 <12681@holbertonstudents.com>
2026-02-02 19:20:48 +0000

    7. Normalize Normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -30,3 +30,15 @@ class Normal:
 
             self.mean = float(mean)
             self.stddev = variance ** 0.5
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score
+        """
+        return z * self.stddev + self.mean
