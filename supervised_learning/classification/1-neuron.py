c70fc05e96e7bac495912b45e094694bb735a6bf
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-02 17:25:38 +0000

    implement private attributes and getters in 1-neuron

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron for binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
