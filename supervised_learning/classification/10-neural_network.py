d896019e0cd72c11454ce812a34d01a00d02df03
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-02 18:32:45 +0000

    10. NeuralNetwork forward propagation

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Module 10-neural_network
+Defines a neural network with one hidden layer performing binary classification
+with forward propagation.
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Neural network with one hidden layer for binary classification."""
+
+    def __init__(self, nx, nodes):
+        """Initialize the neural network with private attributes."""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for W1."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for b1."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for A1."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for W2."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for b2."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for A2."""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates forward propagation of the neural network using sigmoid activation."""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return self.__A1, self.__A2
