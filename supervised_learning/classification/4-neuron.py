83a7e2f7ba7dd023ed1b022078f6f1ea122fe453
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-02 17:33:08 +0000

    add evaluate method to neuron

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron for binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates forward propagation"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates logistic regression cost"""
+        m = Y.shape[1]
+        cost = - (1 / m) * np.sum(
+            Y * np.log(A) +
+            (1 - Y) * np.log(1.0000001 - A)
+        )
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neuron's predictions
+
+        Returns:
+        - prediction (1, m)
+        - cost
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+
+        prediction = np.where(A >= 0.5, 1, 0)
+
+        return prediction, cost
