5ec8c171868c694096a640a0221a5d93e38cd31a
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-02 17:29:10 +0000

    add forward propagation method to neuron

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron for binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates forward propagation of the neuron
+
+        X: numpy.ndarray with shape (nx, m)
+        Updates __A using sigmoid activation
+        Returns: __A
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
