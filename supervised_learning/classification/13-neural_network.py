7043309f7e7ea87ffe6cee342bf3e2d9d870839c
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-02 18:43:29 +0000

    add gradient_descent method to NeuralNetwork

---
+++ supervised_learning/classification/13-neural_network.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""13-neural_network.py"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Neural network with one hidden layer performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates forward propagation using sigmoid activation"""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates the cost using logistic regression"""
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neural network's predictions"""
+        self.forward_prop(X)
+        predictions = np.where(self.__A2 >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A2)
+        return predictions, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Performs one pass of gradient descent on the network"""
+        m = Y.shape[1]
+
+        dZ2 = A2 - Y
+        dW2 = np.dot(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+
+        dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.dot(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
