da9713582eda67f2c72f788d464c86846f7407e3
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-05 09:37:04 +0000

    18. DeepNeuralNetwork forward propagation

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -1,69 +1,75 @@
 #!/usr/bin/env python3
-"""Deep Neural Network class for binary classification"""
+"""Deep Neural Network forward propagation"""
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network for binary classification"""
+    """Defines a deep neural network performing binary classification"""
 
     def __init__(self, nx, layers):
-        """Class constructor"""
+
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
+
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
-
-        for neurons in layers:
-            if not isinstance(neurons, int) or neurons < 1:
-                raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
+        prev = nx
+
         for i in range(self.__L):
-            layer_num = i + 1
 
-            if i == 0:
-                self.__weights['W{}'.format(layer_num)] = np.random.randn(
-                    layers[i], nx) * np.sqrt(2 / nx)
-            else:
-                self.__weights['W{}'.format(layer_num)] = np.random.randn(
-                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
 
-            self.__weights['b{}'.format(layer_num)] = np.zeros((layers[i], 1))
+            nodes = layers[i]
+
+            self.__weights["W{}".format(i + 1)] = (
+                np.random.randn(nodes, prev) * np.sqrt(2 / prev)
+            )
+
+            self.__weights["b{}".format(i + 1)] = np.zeros((nodes, 1))
+
+            prev = nodes
 
     @property
     def L(self):
-        """Number of layers"""
+        """getter for number of layers"""
         return self.__L
 
     @property
     def cache(self):
-        """Cache dictionary"""
+        """getter for cache"""
         return self.__cache
 
     @property
     def weights(self):
-        """Weights dictionary"""
+        """getter for weights"""
         return self.__weights
 
     def forward_prop(self, X):
-        """Calculates forward propagation of the neural network"""
-        self.__cache['A0'] = X
+        """
+        Calculates forward propagation
+        """
 
-        for layer in range(1, self.__L + 1):
-            W = self.__weights['W{}'.format(layer)]
-            b = self.__weights['b{}'.format(layer)]
-            A_prev = self.__cache['A{}'.format(layer - 1)]
+        self.__cache["A0"] = X
+
+        for l in range(1, self.__L + 1):
+
+            W = self.__weights["W{}".format(l)]
+            b = self.__weights["b{}".format(l)]
+            A_prev = self.__cache["A{}".format(l - 1)]
 
             Z = np.matmul(W, A_prev) + b
             A = 1 / (1 + np.exp(-Z))
 
-            self.__cache['A{}'.format(layer)] = A
+            self.__cache["A{}".format(l)] = A
 
         return A, self.__cache
