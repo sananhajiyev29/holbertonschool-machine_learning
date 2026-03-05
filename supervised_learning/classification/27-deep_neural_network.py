7fcd82fc10736be9827a56093ddeef5e190c8644
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-05 11:04:46 +0000

    27. Update DeepNeuralNetwork fix cost 1e-7

--- supervised_learning/classification/27-deep_neural_network.py
+++ supervised_learning/classification/27-deep_neural_network.py
@@ -91,7 +91,7 @@ class DeepNeuralNetwork:
             The cost.
         """
         m = Y.shape[1]
-        cost = -np.sum(Y * np.log(A * 1.0000001)) / m
+        cost = -np.sum(Y * np.log(A + 1e-7)) / m
         return cost
 
     def evaluate(self, X, Y):
