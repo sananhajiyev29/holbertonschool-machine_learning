b9df555916e6ce4f2dddc1bf15448f92a346f735
sananhajiyev29 <12681@holbertonstudents.com>
2026-03-15 18:27:09 +0000

    7. RMSProp

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module that updates a variable using the RMSProp optimization algorithm."""
+
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """Updates a variable using the RMSProp optimization algorithm.
+
+    Args:
+        alpha: the learning rate.
+        beta2: the RMSProp weight.
+        epsilon: small number to avoid division by zero.
+        var: numpy.ndarray containing the variable to be updated.
+        grad: numpy.ndarray containing the gradient of var.
+        s: the previous second moment of var.
+
+    Returns:
+        The updated variable and the new moment, respectively.
+    """
+    s_new = beta2 * s + (1 - beta2) * grad ** 2
+    var_new = var - alpha * grad / (np.sqrt(s_new) + epsilon)
+    return var_new, s_new
