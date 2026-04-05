bacf9ddaa1dac4e5aa5e0fe5b55fb0fbcb8498e8
sananhajiyev29 <12681@holbertonstudents.com>
2026-04-05 22:29:26 +0000

    4. Brightness pycodestyle fix

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -8,7 +8,8 @@ def change_brightness(image, max_delta):
 
     Args:
         image: 3D tf.Tensor containing the image to change.
-        max_delta: the maximum amount the image should be brightened or darkened.
+        max_delta: the maximum amount the image should be brightened
+            or darkened.
 
     Returns:
         The altered image.
