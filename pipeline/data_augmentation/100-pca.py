82ac726dab089876685d064e60a5b573a3d7322a
sananhajiyev29 <12681@holbertonstudents.com>
2026-04-05 22:45:00 +0000

    7. PCA Color Augmentation TF only

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Module that performs PCA color augmentation as in the AlexNet paper."""
 import tensorflow as tf
-import numpy as np
 
 
 def pca_color(image, alphas):
@@ -15,28 +14,31 @@ def pca_color(image, alphas):
         The augmented image.
     """
     image = tf.cast(image, tf.float64)
-    img = image.numpy()
+    shape = tf.shape(image)
+    h = shape[0]
+    w = shape[1]
 
-    img_flat = img.reshape(-1, 3)
+    img_flat = tf.reshape(image, [-1, 3])
     img_norm = img_flat / 255.0
 
-    mean = np.mean(img_norm, axis=0)
+    mean = tf.reduce_mean(img_norm, axis=0, keepdims=True)
     img_centered = img_norm - mean
 
-    cov = np.cov(img_centered.T)
-    eigenvalues, eigenvectors = np.linalg.eigh(cov)
+    n = tf.cast(tf.shape(img_centered)[0] - 1, tf.float64)
+    cov = tf.matmul(tf.transpose(img_centered), img_centered) / n
 
-    idx = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[idx]
-    eigenvectors = eigenvectors[:, idx]
+    eigenvalues, eigenvectors = tf.linalg.eigh(cov)
 
-    delta = np.dot(
-        eigenvectors,
-        (alphas * eigenvalues)
-    )
+    idx = tf.argsort(eigenvalues, direction='DESCENDING')
+    eigenvalues = tf.gather(eigenvalues, idx)
+    eigenvectors = tf.gather(eigenvectors, idx, axis=1)
+
+    alphas = tf.cast(alphas, tf.float64)
+    delta = tf.linalg.matvec(eigenvectors, alphas * eigenvalues)
 
     img_augmented = img_norm + delta
-    img_augmented = np.clip(img_augmented, 0.0, 1.0)
-    img_augmented = (img_augmented * 255.0).reshape(img.shape)
+    img_augmented = tf.clip_by_value(img_augmented, 0.0, 1.0)
+    img_augmented = img_augmented * 255.0
+    img_augmented = tf.reshape(img_augmented, [h, w, 3])
 
-    return tf.cast(tf.convert_to_tensor(img_augmented), tf.uint8)
+    return tf.cast(img_augmented, tf.uint8)
