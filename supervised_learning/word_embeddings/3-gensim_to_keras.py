d0928ce4541c3a0cef7d650c816325c1561b0df1
sananhajiyev29 <12681@holbertonstudents.com>
2026-07-10 06:13:21 +0000

    3. Extract Word2Vec

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module that converts a gensim word2vec model to a keras layer."""
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
@@ -11,4 +12,14 @@ def gensim_to_keras(model):
     Returns:
         The trainable keras Embedding layer.
     """
-    return model.wv.get_keras_embedding(train_embeddings=True)
+    weights = model.wv.vectors
+    vocab_size, vector_size = weights.shape
+
+    layer = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=vector_size,
+        weights=[weights],
+        trainable=True
+    )
+
+    return layer
