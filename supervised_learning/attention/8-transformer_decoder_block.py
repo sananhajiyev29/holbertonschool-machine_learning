e9e89a26faa2b04034f1601b1548c7f874da07b1
sananhajiyev29 <12681@holbertonstudents.com>
2026-07-10 06:52:28 +0000

    7. Transformer Decoder Block

--- supervised_learning/attention/8-transformer_decoder_block.py
+++ supervised_learning/attention/8-transformer_decoder_block.py
@@ -13,26 +13,64 @@ class DecoderBlock(tf.keras.layers.Layer):
         Args:
             dm: the dimensionality of the model.
             h: the number of heads.
-            hidden: the number of hidden units in the fully connected
-                layer.
+            hidden: the number of hidden units in the fully connected layer.
             drop_rate: the dropout rate.
         """
         super().__init__()
         self.mha1 = MultiHeadAttention(dm, h)
         self.mha2 = MultiHeadAttention(dm, h)
-        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+
+        self.dense_hidden = tf.keras.layers.Dense(
+            hidden,
+            activation='relu'
+        )
         self.dense_output = tf.keras.layers.Dense(dm)
-        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm2 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm3 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
         self.dropout3 = tf.keras.layers.Dropout(drop_rate)
 
-    def call(self, x, encoder_output, training, look_ahead_mask,
-             padding_mask):
+    def call(self, x, encoder_output, training,
+             look_ahead_mask, padding_mask):
         """Performs the forward pass of the decoder block.
 
         Args:
-            x: tensor of shape (batch, target_seq_len, dm) containing the
-                input to the decoder block.
+            x: tensor of shape (batch, target_seq_len, dm).
+            encoder_output: tensor of shape
+                (batch, input_seq_len, dm).
+            training: boolean indicating training mode.
+            look_ahead_mask: mask for the first MHA.
+            padding_mask: mask for the second MHA.
+
+        Returns:
+            Tensor of shape (batch, target_seq_len, dm).
+        """
+        attn1, _ = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(x + attn1)
+
+        attn2, _ = self.mha2(
+            out1,
+            encoder_output,
+            encoder_output,
+            padding_mask
+        )
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(out1 + attn2)
+
+        hidden = self.dense_hidden(out2)
+        output = self.dense_output(hidden)
+        output = self.dropout3(output, training=training)
+        out3 = self.layernorm3(out2 + output)
+
+        return out3
