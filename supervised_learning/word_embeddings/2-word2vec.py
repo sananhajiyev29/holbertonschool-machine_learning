147b7e6b39e98d22e545efb720f060dc8c1c874a
sananhajiyev29 <12681@holbertonstudents.com>
2026-07-10 05:58:07 +0000

    2. Train Word2Vec remove double training

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -36,10 +36,4 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         epochs=epochs
     )
 
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=model.epochs
-    )
-
     return model
