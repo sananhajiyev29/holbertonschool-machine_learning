a027b8d391461d32a28f156dfee32da5ed7869e7
sananhajiyev29 <12681@holbertonstudents.com>
2026-07-10 06:57:01 +0000

    8. Transformer Encoder

---
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""Module that creates a transformer encoder."""
+import tensorflow as tf
+import numpy as np
+
+positional_encoding = __import__(
+    '4-positional_encoding'
+).positional_encoding
+EncoderBlock = __import__(
+    '7-transformer_encoder_block'
+).EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """Creates the encoder for a transformer."""
+
+    def __init__(self, N, dm, h, hidden,
+                 input_vocab, max_seq_len, drop_rate=0.1):
+        """Initializes the Encoder.
+
+        Args:
+            N: number of encoder blocks.
+            dm: dimensionality of the model.
+            h: number of attention heads.
+            hidden: number of hidden units in the feed-forward layer.
+            input_vocab: size of the input vocabulary.
+            max_seq_len: maximum possible sequence length.
+            drop_rate: dropout rate.
+        """
+        super().__init__()
+
+        self.N = N
+        self.dm = dm
+
+        self.embedding = tf.keras.layers.Embedding(
+            input_vocab,
+            dm
+        )
+
+        self.positional_encoding = positional_encoding(
+            max_seq_len,
+            dm
+        )
+
+        self.blocks = [
+            EncoderBlock(dm, h, hidden, drop_rate)
+            for _ in range(N)
+        ]
+
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """Performs the forward pass of the encoder.
+
+        Args:
+            x: tensor of shape (batch, input_seq_len).
+            training: boolean indicating training mode.
+            mask: mask for multi-head attention.
+
+        Returns:
+            Tensor of shape (batch, input_seq_len, dm).
+        """
+        seq_len = tf.shape(x)[1]
+
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+
+        x += self.positional_encoding[:seq_len]
+
+        x = self.dropout(x, training=training)
+
+        for block in self.blocks:
+            x = block(x, training, mask)
+
+        return x
