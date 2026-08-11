a95cadf6e73876ccf7cce73fdd0168afea4636d5
sananhajiyev29 <12681@holbertonstudents.com>
2026-08-11 05:37:02 +0000

    0. Load the Environment

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module that loads the FrozenLakeEnv environment from gymnasium."""
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """Loads the pre-made FrozenLakeEnv environment from gymnasium.
+
+    Args:
+        desc: either None or a list of lists containing a custom
+            description of the map to load for the environment.
+        map_name: either None or a string containing the pre-made map
+            to load.
+        is_slippery: boolean to determine if the ice is slippery.
+
+    Returns:
+        The environment.
+    """
+    env = gym.make(
+        'FrozenLake-v1',
+        desc=desc,
+        map_name=map_name,
+        is_slippery=is_slippery
+    )
+    return env
---
+++ reinforcement_learning/q_learning/README.md
@@ -0,0 +1,1 @@
+This project contains tasks related to Q-learning and reinforcement learning.
