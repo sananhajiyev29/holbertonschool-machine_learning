bddc5d5e70781868ff271fd00e1c40e77c352634
sananhajiyev29 <12681@holbertonstudents.com>
2026-08-17 14:38:06 +0000

    0. Monte Carlo

---
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Module that performs the Monte Carlo algorithm."""
+import numpy as np
+
+
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
+                gamma=0.99):
+    """Performs the Monte Carlo algorithm.
+
+    Args:
+        env: environment instance.
+        V: numpy.ndarray of shape (s,) containing the value estimate.
+        policy: function that takes in a state and returns the next
+            action to take.
+        episodes: total number of episodes to train over.
+        max_steps: maximum number of steps per episode.
+        alpha: the learning rate.
+        gamma: the discount rate.
+
+    Returns:
+        V, the updated value estimate.
+    """
+    for episode in range(episodes):
+        state, _ = env.reset()
+        episode_data = []
+
+        for step in range(max_steps):
+            action = policy(state)
+            new_state, reward, terminated, truncated, _ = env.step(action)
+            episode_data.append((state, reward))
+
+            if terminated or truncated:
+                break
+
+            state = new_state
+
+        episode_data = np.array(episode_data, dtype=int)
+        G = 0
+
+        for state, reward in reversed(episode_data):
+            G = gamma * G + reward
+
+            if state not in episode_data[:episode_data.shape[0], 0]:
+                V[state] = V[state] + alpha * (G - V[state])
+
+        for i, (state, reward) in enumerate(episode_data):
+            if state not in episode_data[:i, 0]:
+                V[state] = V[state] + alpha * (G - V[state])
+
+    return V
---
+++ reinforcement_learning/temporal_difference/README.md
@@ -0,0 +1,1 @@
+This project contains tasks related to temporal difference learning.
