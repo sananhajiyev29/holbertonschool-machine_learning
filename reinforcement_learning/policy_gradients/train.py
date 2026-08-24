f9ebac2afa4dcd98c908700707ca7c71576ce235
sananhajiyev29 <12681@holbertonstudents.com>
2026-08-24 18:56:34 +0000

    3. Animate iteration

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -4,7 +4,7 @@ import numpy as np
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     """Implements a full training using policy gradients.
 
     Args:
@@ -12,6 +12,8 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         nb_episodes: number of episodes used for training.
         alpha: the learning rate.
         gamma: the discount factor.
+        show_result: if True, render the environment every 1000
+            episodes.
 
     Returns:
         All values of the score (sum of all rewards during one episode).
@@ -29,6 +31,9 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         score = 0
 
         while True:
+            if show_result and episode % 1000 == 0:
+                env.render()
+
             action, gradient = policy_gradient(state, weight)
             next_state, reward, terminated, truncated, _ = env.step(action)
 
