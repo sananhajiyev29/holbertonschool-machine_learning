bf6e59d55c53446b0bf0753548bb27f49afe244d
sananhajiyev29 <12681@holbertonstudents.com>
2026-08-24 18:48:17 +0000

    0. Monte Carlo

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -21,27 +21,26 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
         V, the updated value estimate.
     """
     for episode in range(episodes):
-        state, _ = env.reset()
+        state = env.reset()[0]
         episode_data = []
 
         for step in range(max_steps):
             action = policy(state)
-            new_state, reward, terminated, truncated, _ = env.step(action)
+            next_state, reward, terminated, truncated, _ = env.step(action)
             episode_data.append((state, reward))
 
             if terminated or truncated:
                 break
 
-            state = new_state
+            state = next_state
 
-        episode_data = np.array(episode_data, dtype=int)
         G = 0
+        episode_data = episode_data[::-1]
 
-        for i in reversed(range(len(episode_data))):
-            state, reward = episode_data[i]
+        for i, (state, reward) in enumerate(episode_data):
             G = gamma * G + reward
 
-            if state not in episode_data[:i, 0]:
+            if state not in [x[0] for x in episode_data[i + 1:]]:
                 V[state] = V[state] + alpha * (G - V[state])
 
     return V
